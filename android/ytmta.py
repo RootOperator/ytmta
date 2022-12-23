5aafa77d457c404af85474a24054dddffed88c08
RootOperator <20143055+RootOperator@users.noreply.github.com>
2022-12-23 17:46:42 +0100

    .

--- android/ytmta.py
+++ android/ytmta.py
@@ -3,14 +3,14 @@ import asyncio
 import websockets
 
 
-async def send(websocket):
+async def open(websocket):
     async for message in websocket:
         os.system(f"am start -a android.intent.action.VIEW -d \"{message}\"")
         await websocket.close()
 
 
 async def main():
-    async with websockets.serve(send, port=9876):
+    async with websockets.serve(open, port=9876):
         await asyncio.Future()
 
 asyncio.run(main())
